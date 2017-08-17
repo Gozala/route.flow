848d22601eb16908e12ed3aa9c4693a9f5d065c1
Irakli Gozalishvili <rfobic@gmail.com>
2017-08-17 16:51:06 -0700

    Fix tests

--- test/Route.js
+++ test/Route.js
@@ -6,52 +6,45 @@ import * as Integer from "integer.flow"
 
 test("test baisc", async test => {
   test.isEqual(typeof Route, "object")
-  test.isEqual(typeof Route.Root, "object")
-  test.isEqual(typeof Route.segment, "function")
-  test.isEqual(typeof Route.format, "function")
-  test.isEqual(typeof Route.concat, "function")
 
-  test.isEqual(typeof Route.param, "function")
   test.isEqual(typeof Route.String, "object")
   test.isEqual(typeof Route.Integer, "object")
   test.isEqual(typeof Route.Float, "object")
 
+  test.isEqual(typeof Route.Root, "object")
+  test.isEqual(typeof Route.segment, "function")
+
+  test.isEqual(typeof Route.concat, "function")
+  test.isEqual(typeof Route.param, "function")
   test.isEqual(typeof Route.query, "function")
+
   test.isEqual(typeof Route.parse, "function")
-  test.isEqual(typeof Route.parseHash, "function")
   test.isEqual(typeof Route.parsePath, "function")
+  test.isEqual(typeof Route.parseHash, "function")
+
+  test.isEqual(typeof Route.format, "function")
+  test.isEqual(typeof Route.formatPath, "function")
+  test.isEqual(typeof Route.formatHash, "function")
 })
 
 test("test route", async test => {
-  const userCommentCodec = Route.segment("user")
+  const userComment = Route.segment("user")
     .param(Route.String)
     .segment("comments")
     .param(Route.Integer)
 
-  class Comment {
-    name: string
-    id: number
-    static new(name: string, id: number): Comment {
-      return new Comment(name, id)
-    }
-    constructor(name: string, id: number) {
-      this.name = name
-      this.id = id
-    }
-  }
-
-  test.deepEqual(Route.parse(userCommentCodec, "/user/bob/comments/42", {}), [
-    "bob",
-    42
-  ])
+  test.deepEqual(
+    Route.parsePath(userComment, { pathname: "user/bob/comments/42" }),
+    ["bob", 42]
+  )
 
   test.deepEqual(
-    Route.parse(userCommentCodec, "/user/bob/comment/42", {}),
+    Route.parsePath(userComment, { pathname: "/user/bob/comment/42" }),
     null
   )
 
   test.deepEqual(
-    Route.format(userCommentCodec, "Jack", Integer.truncate(15)),
-    "/user/Jack/comments/15"
+    Route.formatPath(userComment, "Jack", Integer.truncate(15)),
+    "user/Jack/comments/15"
   )
 })
